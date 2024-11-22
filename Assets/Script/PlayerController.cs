8f0ce2b9bf7168ca1bdfb9ad958edea7a5208910
Justin-bit25 <justinr777@nycstudents.net>
2024-11-22 09:01:15 -0500

    part 9 complete

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,21 +7,29 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
    public InputAction MoveAction;
+   Rigidbody2D rigidbody2d;
+   Vector2 move;
 
 
    // Start is called before the first frame update
    void Start()
    {
-      MoveAction.Enable();  
+      MoveAction.Enable();
+      rigidbody2d = GetComponent<Rigidbody2D>();
    }
 
 
    // Update is called once per frame
    void Update()
    {
-     Vector2 move = MoveAction.ReadValue<Vector2>();
-     Debug.Log(move);
-     Vector2 position = (Vector2)transform.position + move * 0.1f * Time.deltaTime;
-     transform.position = position;
+      move = MoveAction.ReadValue<Vector2>();
+      Debug.Log(move);
+   }
+
+   void FixedUpdate()
+   {
+
+      Vector2 position = (Vector2)rigidbody2d.position + move * 0.1f * Time.deltaTime;
+     rigidbody2d.MovePosition(position);
    }
 }

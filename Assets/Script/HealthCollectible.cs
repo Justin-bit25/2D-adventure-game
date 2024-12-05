5c4171cf974f5b0e4c4aec2ef6b5cf8b652c1d68
Justin-bit25 <justinr777@nycstudents.net>
2024-12-05 09:21:01 -0500

    part 13 complete

--- Assets/Script/HealthCollectible.cs
+++ Assets/Script/HealthCollectible.cs
@@ -5,17 +5,17 @@ using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
-   void OnTriggerEnter2D(Collider2D other)
-   {  
-      PlayerController controller = other.GetComponent<PlayerController>();
 
 
-      if (controller != null)
-      {
-         controller.ChangeHealth(1);
-         Destroy(gameObject);
-      }
- 
+void OnTriggerEnter2D(Collider2D other)
+{
+   PlayerController controller = other.GetComponent<PlayerController>();
+
+
+   if (controller != null && controller.health< controller.maxHealth)
+   {
+       controller.ChangeHealth(1);
+       Destroy(gameObject);
    }
-
+}
 }
